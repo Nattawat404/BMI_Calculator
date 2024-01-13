7702cd77c38a6fe42a2f4a5143f9c1f41054d584
Nattawat404 <nthueakthao@gmail.com>
2024-01-13 17:30:14 +0700

    update

--- BMICalculator.py
+++ BMICalculator.py
@@ -14,7 +14,36 @@ class HightScreen(Screen):
      pass
 class WeightScreen(Screen):
      pass
-class BMIScreen(Screen,FloatLayout):
+class BMIScreen(Screen,FloatLayout):     
+        def __init__(self,**kwargs):
+            super(BMIScreen, self).__init__(**kwargs)
+        def calc(self,m,s):
+                if m != "" and s != "":
+                                try:
+                                        return str(round((float(s)/(float(m)**2)),2))
+                                except Exception:
+                                        return "Error"
+                else:
+                        return ""
+        def weight(self,result):
+                if result == "":
+                        return ""      
+                else:
+                        try:
+                                if  float(result)<= 18.5:
+                                        return "Underweight"
+                                elif  float(result)<= 25:
+                                        return "Normal"
+                                elif  float(result)<= 30:
+                                        return "Overweight"
+                                elif  float(result)<= 35:
+                                        return "Obese"
+                                else:
+                                        return "Extremely obese"
+                        except Exception:
+                                return "Error"
+
+
         
 class ScreenManagement(ScreenManager):
     pass
