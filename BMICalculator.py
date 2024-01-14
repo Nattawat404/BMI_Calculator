a0f4136030320ab3e08f23c32236a4f6b86281d0
Nattawat404 <nthueakthao@gmail.com>
2024-01-14 16:25:23 +0700

    update

--- BMICalculator.py
+++ BMICalculator.py
@@ -1,50 +1,53 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
+from kivy.core.window import Window
 
 
-class MainScreen(Screen):
-      pass
-class GenderScreen(Screen):
-      pass
-class AgeScreen(Screen):
-     pass
-class HightScreen(Screen):
-     pass
-class WeightScreen(Screen):
-     pass
-class BMIScreen(Screen,FloatLayout):     
+
+
+class MainScreen(GridLayout):
         def __init__(self,**kwargs):
-            super(BMIScreen, self).__init__(**kwargs)
-        def calc(self,m,s):
-                if m != "" and s != "":
-                                try:
-                                        return str(round((float(s)/(float(m)**2)),2))
-                                except Exception:
-                                        return "Error"
-                else:
-                        return ""
-        def weight(self,result):
-                if result == "":
-                        return ""      
-                else:
-                        try:
-                                if  float(result)<= 18.5:
-                                        return "Underweight"
-                                elif  float(result)<= 25:
-                                        return "Normal"
-                                elif  float(result)<= 30:
-                                        return "Overweight"
-                                elif  float(result)<= 35:
-                                        return "Obese"
-                                else:
-                                        return "Extremely obese"
-                        except Exception:
-                                return "Error"
-        
-class ScreenManagement(ScreenManager):
-    pass
-if __name__ == "__main__":
-        BMIScreen(Screen,FloatLayout).run()
+            super(MainScreen, self).__init__(**kwargs)
+            self.cols = 1
+
+            self.top_grid=GridLayout()
+            self.top_grid.cols=1
+
+            self.top_grid.add_widget(Label(text="Welcome to BMI Calculator"))
+            self.top_grid.add_widget(Label(text="Please enter your Name: "))
+            self.name = TextInput(multiline=False)
+            self.top_grid.add_widget(self.name)
+            self.add_widget(self.top_grid)
+
+            self.start=Button(text="Start",
+                              font_size=40,
+                              size_hint_y=None,
+                              height=50
+                              )
+            self.start.bind(on_press=self.press)
+            self.add_widget(self.start)
+
+        def press(self):
+             pass
+
+# class GenderScreen(Screen):
+#       pass
+# class AgeScreen(Screen):
+#      pass
+# class HightScreen(Screen):
+#      pass
+# class WeightScreen(Screen):
+#      pass
+# class BMIScreen(Screen):     
+class MyApp(App):
+       def build(sself):
+        #       Window.clearcolor=(1,1,1,1 )
+              return MainScreen()
+if __name__ =="__main__":
+         MyApp().run()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
-WeightScreen
+class GenderScreen(Screen):
+#       pass
+# class AgeScreen(Screen):
+#      pass
+# class HightScreen(Screen):
+#      pass
+# class WeightScreen(Screen):
+#      pass
+# class BMIScreen(Screen):     
