88bea9b3e4fefe0330a8ab3318aa4010d62a928e
Nattawat404 <nthueakthao@gmail.com>
2024-01-14 19:33:31 +0700

    update

--- BMICalculator.py
+++ BMICalculator.py
@@ -1,10 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
